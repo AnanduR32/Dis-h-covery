0f630be80bad7151ded718d155f6508f543bdb53
Anandu R <ananduraj32@gmail.com>
2025-02-09 13:04:09 +0530

    Initial setup done

---
+++ dis-h-covery/src/Services/meal-db-endpoint.service.ts
@@ -0,0 +1,11 @@
+import { API_ENDPOINTS } from "../Shared/Constants";
+
+export const fetchMealsBySearch = async (query: string) => {
+  const response = await fetch(`${API_ENDPOINTS.SEARCH}${query}`);
+  return response.json();
+};
+
+export const fetchMealById = async (id: string) => {
+  const response = await fetch(`${API_ENDPOINTS.LOOKUP}${id}`);
+  return response.json();
+};
---
+++ dis-h-covery/src/Shared/Constants.ts
@@ -0,0 +1,10 @@
+export const API_BASE_URL = "https://www.themealdb.com/api/json/v1/1";
+
+// Endpoints
+export const API_ENDPOINTS = {
+  SEARCH: `${API_BASE_URL}/search.php?s=`,  // Search meals by name
+  LOOKUP: `${API_BASE_URL}/lookup.php?i=`,  // Get meal details by ID
+  RANDOM: `${API_BASE_URL}/random.php`,    // Get a random meal
+  CATEGORY: `${API_BASE_URL}/categories.php`,  // Get all meal categories
+  FILTER_BY_CATEGORY: `${API_BASE_URL}/filter.php?c=`, // Get meals by category
+};
